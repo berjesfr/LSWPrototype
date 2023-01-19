73072d3eff32d1e6238d04c498b60ed2471ef894
berjsfr <gustavo.borgesfr@gmail.com>
2023-01-19 20:18:07 -0300

    Update GUI assets and add animated NPC

--- Assets/src/NPCInteraction.cs
+++ Assets/src/NPCInteraction.cs
@@ -9,17 +9,22 @@ public class NPCInteraction : MonoBehaviour
 
     private bool m_PlayerNearby;
     public GameObject m_ShopPanel;
+    public GameObject m_Player;
 
     private void Awake()
     {
-        m_ShopPanel.SetActive(false);
+        m_ShopPanel.SetActive(true);
     }
 
     private void Update()
     {
+        Vector2 xDelta = m_Player.transform.position - gameObject.transform.position;
+        transform.rotation = Quaternion.Euler(0.0f, xDelta.x < 0 ? 0.0f : 180.0f, 0.0f);
+
         if (m_PlayerNearby && Input.GetKeyDown(KeyCode.E)) {
             m_ShopPanel.SetActive(true);
         }
+    
         if (m_ShopPanel.activeSelf && Input.GetKeyDown(KeyCode.Escape)) {
             m_ShopPanel.SetActive(false);
         }
