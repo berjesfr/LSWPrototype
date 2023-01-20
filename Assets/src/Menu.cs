a27df487e01cf7bb9e24da5d77bf10ffc145b53a
berjsfr <gustavo.borgesfr@gmail.com>
2023-01-20 03:17:26 -0300

    Add main menu and final adjustments

---
+++ Assets/src/Menu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{  
+    public void RunGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
